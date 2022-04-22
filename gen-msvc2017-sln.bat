eb48182a74935eac0cbc37f7a48aa87e661b5e56
Alexander Martynov <amart@mail.ru>
2022-04-22 21:04:45 +0300

    Added project files

---
+++ gen-msvc2017-sln.bat
@@ -0,0 +1,4 @@
+umba-subst-macros --overwrite --raw ^
+--set="Visual Studio Version 16:Visual Studio 15" ^
+--set="VisualStudioVersion = 16.:VisualStudioVersion = 15" ^
+gtest-msvc2019.sln gtest-msvc2017.sln
